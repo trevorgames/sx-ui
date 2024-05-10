6b6ca80936f80b07570aa742e793337498718931
Howader <howader@outlook.com>
2024-05-10 18:37:12 +0800

    fix rpc provider

--- src/helpers/provider.ts
+++ src/helpers/provider.ts
@@ -1,6 +1,11 @@
 import { StaticJsonRpcProvider } from '@ethersproject/providers';
 
-const providers: Record<number, StaticJsonRpcProvider | undefined> = {};
+const trevorSepliaRpcUrl = `https://dev-rpc.trevorgames.xyz`;
+const trevorSepoliaProvider = new StaticJsonRpcProvider({ url: trevorSepliaRpcUrl, timeout: 25000 }, 689388);
+
+const providers: Record<number, StaticJsonRpcProvider | undefined> = {
+  689388: trevorSepoliaProvider
+};
 
 export function getProvider(networkId: number): StaticJsonRpcProvider {
   const url = `https://rpc.snapshotx.xyz/${networkId}`;
